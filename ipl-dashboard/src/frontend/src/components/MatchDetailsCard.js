776f1d4c537334d42806bc5ab2ab51f26457e9ea
Rishabh <androme.dianpaul@gmail.com>
2021-05-15 19:14:40 +0530

    frontend

---
+++ ipl-dashboard/src/frontend/src/components/MatchDetailsCard.js
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+export const MatchDetailsCard = ()=>{
+  return (
+    <div className="MatchDetailsCard">
+      <h2>Latest Matches</h2>
+      <h4>Matche Details</h4>
+    </div>
+  );
+}
+
